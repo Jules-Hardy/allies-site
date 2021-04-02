9f6cd6ed4015ba327baafce711c96a5fec5e38a0
45738070 <hardyjules@icloud.com>
2021-04-02 14:34:06 +0200

    réorganisation

---
+++ sitevitrine/includes/head.php
@@ -0,0 +1,6 @@
+<head>
+  <!-- En-tête de la page -->
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../ressources/css/main.css" />
+    <title> Infinite Measures | Tests psychotechniques </title>
+</head>
---
+++ sitevitrine/pages/accueil.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<?php include '../includes/head.php'; ?>
+
+
+    <body>
+      <?php include '../includes/header.php'; ?>
+
+
+        <div id="contenu">
+          <img id="pilotes" src="../ressources/images/antoine.png">
+      	  <br><br>
+
+      <div id="carte">
+
+
+      		<div id="presentation">
+      	    <p>Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino spectante, convictus cffodicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.
+      	    </p>
+      		</div>
+      		<br><br><br>
+
+      	    <h3>Nos produits:</h3>
+      	    <br>
+
+      	<div id="troisetapes">
+
+      		<div class="nosproduit">
+      			<div id="iconegauche">
+      	    	<img class="icones" src="capteur.png">
+      	    	<h4>Des capteurs</h4>
+      	    	</div>
+      	    	<div id="textegauche">
+      	    	<p>Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse javajava</p>
+      	    	</div>
+      	    </div>
+
+
+      		<div class="nosproduits">
+      			<div id="iconecentre">
+          		<img class="icones" src="analyse.png">
+      	    	<h4>Des analyses</h4>
+      	    	</div>
+      	    	<div id="textecentre">
+      	   		<p>Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque mittendus</p>
+      	   		</div>
+      	   	</div>
+
+
+      		<div class="nosproduits">
+      			<div id="iconedroit">
+          		<img class="icones" src="resultat.png">
+      	    	<h4>Des résultats</h4>
+      	    	</div>
+      	    	<div id="textedroite">
+      	     	<p>Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse</p>
+      	     	</div>
+      	    </div>
+
+      	</div>
+
+      	<br>
+
+      	    <img id="defileavion" src="defileavion.png">
+
+      		<br><br><br><br>
+      		<h2>Nos clients:</h2>
+      		<br>
+
+
+      	<div id="nosclients">
+      		<img id="logo-isep" src="logo-isep.png">
+      		<img id="infinite-measures"  src="infinite-measures.png">
+      		<img id="logo-armee" src="logo-armee.png">
+      	</div>
+      </div>
+        </div>
+        <?php include '../includes/footer.php'; ?>
+
+    </body>
+</html>
