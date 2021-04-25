336ab2afcdcb0fbce4f78e946e00269197db4384
LaurineR <74404877+LaurineR@users.noreply.github.com>
2021-04-25 18:18:45 +0200

    ajouts

---
+++ sitevitrine/pages/mon-profil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<?php include '../includes/head.php'; ?>
+
+
+<body>
+  <?php include '../includes/header.php'; ?>
+
+
+
+
+  <div id="contenu">
+  	<img id="pimg-profil" scr="../ressources/images/image-pp.png">
+
+  </div>
+
+
+
+
+
+   <?php include '../includes/footer.php'; ?>
+
+</body>
+
+</html>
