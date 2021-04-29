4eac906e518c35b2a74c7a3a3e9a91dacc8786e2
LaurineR <74404877+LaurineR@users.noreply.github.com>
2021-04-29 12:17:19 +0200

    bouton

--- sitevitrine/pages/mon-profil.php
+++ sitevitrine/pages/mon-profil.php
@@ -5,14 +5,15 @@
 	<body>
   		<?php include '../includes/header.php'; ?>
 
-  <div id="contenu-profil">
-
+  <div id="image_profil">
   	<img id="img-profil" src="../ressources/images/image-pp.jpg">
+  </div>
 
-  	<button class="modifier"
-  			type="button">
-  		Modifier
-  	</button>
+  <div id="bouton_modifier">
+  	<a href="" id="bouton"><button class="bouton_modifier">MODIFIER</button></a>
+  		<!--<button class"="but_modif"> Modifier</button>-->
+  </div>
+
 
   	<p>
   		Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam
@@ -51,12 +52,6 @@
           mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo,
           ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.s
   	</p>
-
-
-  	
-
-  </div>
-
 
 
 
