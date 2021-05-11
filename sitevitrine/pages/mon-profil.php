20a21facfb8d299c46c258b75570d2ef1802cf0d
LaurineR <74404877+LaurineR@users.noreply.github.com>
2021-05-11 11:05:42 +0200

    fin de la page mon profil

--- sitevitrine/pages/mon-profil.php
+++ sitevitrine/pages/mon-profil.php
@@ -49,46 +49,6 @@
   </div>
 
 
-  	<p>
-  		Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam
-          Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse,
-          exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de
-          fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.Circa hos
-          dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino
-          spectante, convictus cffodicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque
-          mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo,
-          ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.
-          Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam
-          Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse,
-          exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de
-          fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.Circa hos
-          dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino
-          spectante, convictus cffodicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque
-          mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo,
-          ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.
-          Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam
-          Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse,
-          exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de
-          fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.Circa hos
-          dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino
-          spectante, convictus cffodicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque
-          mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo,
-          ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.
-          Circa hos dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam
-          Maximino spectante, convictus codicem noxiarum artium nondum per aetatem firmato consilio descripsisse,
-          exulque mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de
-          fumo, ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.Circa hos
-          dies Lollianus primae lanuginis adulescens, Lampadi filius ex praefecto, exploratius causam Maximino
-          spectante, convictus cffodicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque
-          mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo,
-          ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.s
-            spectante, convictus cffodicem noxiarum artium nondum per aetatem firmato consilio descripsisse, exulque
-          mittendus, ut sperabatur, patris inpulsu provocavit ad principem, et iussus ad eius comitatum duci, de fumo,
-          ut aiunt, in flammam traditus Phalangio Baeticae consulari cecidit funesti carnificis manu.s
-  	</p>
-
-
-
 
    		<?php include '../includes/footer.php'; ?>
 	</body>
