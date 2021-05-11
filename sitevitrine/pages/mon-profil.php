dcd2a5a9fb6519bb96fce3bc6aa203aee9cadf43
LaurineR <74404877+LaurineR@users.noreply.github.com>
2021-05-11 10:59:37 +0200

    hop hop

--- sitevitrine/pages/mon-profil.php
+++ sitevitrine/pages/mon-profil.php
@@ -16,35 +16,35 @@
   <div id="remplir_champs">
   	<div id="remplir_prenom">
   		<p>Prénom</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_nom">
   		<p>Nom</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_age">
   		<p>Âge</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_tel">
   		<p>Téléphone</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_mail">
   		<p>Adresse mail</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_adresse">
   		<p>Adresse postale</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_ville">
   		<p>Ville</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
  	 <div id="remplir_poste">
   		<p>Code Postal</p>
-  		<input type="text" />
+  		<input type="text" size="80"/>
  	 </div>
   </div>
 
