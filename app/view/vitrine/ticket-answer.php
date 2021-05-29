bc169d8f57966873b95d9b4adbdc391c71b25892
45738070 <hardyjules@icloud.com>
2021-05-29 23:40:17 +0200

    ticket answer

--- app/view/vitrine/ticket-answer.php
+++ app/view/vitrine/ticket-answer.php
@@ -1,16 +1,13 @@
 <?php $title = 'Ticket - Répondre'; ?>
 <?php ob_start(); ?>
+<img id="pilotes" src="../ressources/images/pilotes.png">
 
 <div id="carte_FAQ">
-  <div class="images">
-    <img src="../ressources/images/Stylo.jpg" width="100%" height="200%" alt="photo de fond">
-
-  </div>
 
   <div id="q_r">
     <!-- essai -->
     <h1 id="titre2"> Répondre au ticket #1</h1>
-    <h2> Titre du ticket <span style="color:red"><small>Fermer le ticket</small></span></h2>
+    <h2> Titre du ticket <span style="color:red"><small><a href=""> Fermer le ticket</a></small></span></h2>
     <p class="question">
       <b>Administrateur</b> - le 12 mai 2021
       <br>
@@ -26,8 +23,7 @@
 
   </div>
   <form action="" method="POST" id="fo">
-    <div
-      style="display: flex;width: 100%;height: auto;padding: 30px;background-color: white;border-top: solid 1px grey;">
+    <div style="display: flex;height: auto;padding: 20px;background-color: white;border-top: solid 1px grey;">
       <textarea type="text" name="message"
         style="flex-grow: 10;order: 0;margin-right: 30px;padding: 15px;overflow: auto;border-radius: 6px;"
         placeholder="Votre réponse"></textarea>
